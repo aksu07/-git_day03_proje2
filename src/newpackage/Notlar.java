22e079c2d44fa6e62750bcb9a6e1fecb23116634
aksu07 <abdullatifaksu07@gmail.com>
2020-07-06 23:28:08 -0400

    son commit

---
+++ src/newpackage/Notlar.java
@@ -0,0 +1,57 @@
+package newpackage;
+
+public class Notlar {
+
+		/*
+		 * /*
+			 * Kullandiklarimiz => Git commands 
+		git add . => her seyi staging (sahne)alanina ekliyor
+		git commit -m "mesaj" => onlari localda (bilgisayarimiz) kaydediyor / kimlik kazandiriyor.
+		git push => remote a(Github) gonderiyor
+		======================================================
+		git status => her zaman durum bilgisini verir ve hangi asama da nerede oldugunuzu belirler
+		git log => gecmis bilgileri verir
+		======================================================
+		Remote da her hangi bir degisiklik varsa onlari 
+		git fetch => bu sekilde kandi bilgisayarimiza (local repo) getiririz
+		git merge => onlari kendi localimize birlestiririz
+		git pull => git fetch ve git merge un yaptigini tek seferde yapar
+		==========================================================
+		git branch isim=> yeni bir branch (dal) olusturmaya yarar
+		git branch => hangi branch te oldugumuzu gosterir
+		git checkout => yeni baska bir branch gecmeye yarar
+		git branch -D isim => tanimlanan branchi siler
+		============================================================
+		git push --set-upstream origin isim => remote ta yeni branch olusturur veya hali hazirda varsa ona ilk kez tum bilgileri yollar
+		============================================================
+		Simdi yeni bir proje olusturalim 
+		hem local de hemde remote ta
+		git_proje03
+		bugun pull request => Github tan farkli bir daldan master a yapilan degisiklikleri cekecegiz 
+		stash yapmayi gorecegiz
+		collaboration yapmayi gorecegiz
+		.gitignore file olusturmayi gorecegiz
+		=========================================
+		projeler olusturuldu
+		ClassOne => 
+		degisiklikler kaydedildi
+		properties file dan location dan => projenin path => adresini aldik
+		command prompt dan cd C:\Users\sam\eclipse-workspace\git_proje04
+		git init 
+		git add .
+		git commit -m "ilk commit master branchten"
+		Eger remote ta isek ve kendi branchimizden remote mastera bis sey aktarmak istersek, Pull request yapmamiz gerekir
+		PR => kod cekme talebi
+		yaptiginiz degisiklikleri add etmezseniz ve git stash yaparsaniz, sizin bir once commitinize geri doner ve yaptiginiz son degisiklikleri kaydeder
+		git stash => yazdiginiz kodlari kaydeder ve eski(bir onceki) commitinize geri doner.
+		uzerine yaptiginiz degisiklikleri git stash apply stash@{0}
+		command ile bir birine merge edebilirsiniz
+		.gitignore => bu file icine neleri koyarsaniz onlari yok ediyor 
+		Bu dosya icerine hangi file veya folder i koyarsaniz onu sakliyor
+		Yukaridaki tum degisiklikleri sizdekileri silme pahasina degistirmek isterseniz => git reset --hard origin/master
+			 */
+		
+		
+
+	}
+
